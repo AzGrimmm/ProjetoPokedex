0dabfe0313718f032f7970e123910557581a58bb
Bruno H C Jesus <149212642+AzGrimmm@users.noreply.github.com>
2024-01-25 15:54:46 -0300

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,20 @@
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 
 function ConvertPokemonToLi(pokemon){
     return `
         <li class="pokemon">
-            <span class="number">#001</span>
+            <span class="number">#${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    <li class="type"></li>
+                    ${convertPokemonTypesToLi(pokemon.types).join(' ')}
                 </ol>
 
-                <img src="#" alt="${pokemon.name}">
+                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
             </div>
         </li>
     `
@@ -21,4 +25,5 @@ const pokemonList = document.getElementById('pokemonList')
     pokeApi.getPokemons().then( (pokemons = []) => {
         pokemonList.innerHTML += pokemons.map(ConvertPokemonToLi).join('')
     })
-    .catch( (error) => console.error(error))
+
+
